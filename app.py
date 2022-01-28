fddc88978287c78edcaf306766e991bbae071ef6
dylman123 <dylanklein29@gmail.com>
2022-01-28 17:30:58 +1100

    modified target hostname for redis in flask app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import redis
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+cache = redis.Redis(host='my-redis-svc.default.svc.cluster.local', port=6379)  # set host='redis' for localhost development
 
 def get_hit_count():
     retries = 5
